a25881ee6495075b48c968679ae8cf146cab64ab
takasqr <95556025+takasqr@users.noreply.github.com>
2024-01-22 21:42:45 +0900

    GTM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,24 @@ export default defineNuxtConfig({
     // Remove it if you don't use Plausible analytics
     // https://github.com/nuxt-modules/plausible
     '@nuxtjs/plausible'
-  ]
+  ],
+  app: {
+    head: {
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','${process.env.GTM_ID}');` 
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          tagPosition: 'bodyOpen'
+        }
+      ]
+    }
+  }
 })
