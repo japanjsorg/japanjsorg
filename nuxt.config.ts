3e8ddacd02dd0feda1ea50ad934b627c26a88bb9
takasqr <95556025+takasqr@users.noreply.github.com>
2024-03-01 22:25:08 +0900

    OGP 設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,21 @@ export default defineNuxtConfig({
   ],
   app: {
     head: {
+      meta: [
+        { name: 'title', content: 'JapanJS' },
+        { name: 'description', content: 'JapanJS は日本特有の機能を提供するライブラリです。 カナ変換、都道府県の一覧データなど、日本特有のめんどくさい実装を肩代わりします。Tree Shaking なライブラリなのでバンドルサイズが重くなりません。' },
+        // Open Graph
+        { property: 'og:url', content: 'https://japanjs.org' },
+        { property: 'og:title', content: 'JapanJS' },
+        { property: 'og:description', content: 'JapanJS は日本特有の機能を提供するライブラリです。 カナ変換、都道府県の一覧データなど、日本特有のめんどくさい実装を肩代わりします。Tree Shaking なライブラリなのでバンドルサイズが重くなりません。' },
+        { property: 'og:image', content: 'https://japanjs.org/ogp_image.jpg' },
+        // Twitter Card
+        { name: 'twitter:card', content: 'summary_large_image' },
+        { name: 'twitter:site', content: '@takasqr' },
+        { name: 'twitter:creator', content: '@takasqr' },
+        // サムネイル指定
+        { name: 'thumbnail', content: 'https://japanjs.org/ogp_image.jpg' }
+      ],
       script: [
         {
           innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
